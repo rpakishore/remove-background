52d8cd6b4245949e622f32172c1c57994f605906
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2025-05-07 17:11:57 -0700

    Update __init__.py

--- src/rmbg/__init__.py
+++ src/rmbg/__init__.py
@@ -4,17 +4,3 @@
 
 from rich.console import Console
 from rich.panel import Panel
-
-from .utils import log
-
-
-def debug(status=False):
-    """Import this in a new module and enable debug to use debug"""
-    if status:
-        log.setLevel(10)  # debug
-        log.debug(f"Debug Mode: {status}")
-    else:
-        log.setLevel(20)  # info
-
-
-debug(status=False)
